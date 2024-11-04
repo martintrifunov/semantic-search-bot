2b32c622721211c80b0721395ceb4cce32e4e5f6
martintrifunov <martintrifunov@proton.me>
2024-11-04 15:28:09 +0100

    Configure mongodb & connect to mongodb atlas

--- apps/server/app.js
+++ apps/server/app.js
@@ -3,16 +3,20 @@ import { semanticSearchController } from "./src/controllers/semanticSearchContro
 import corsOptions from "./src/config/corsOptions.js";
 import cors from "cors";
 import { answersController } from "./src/controllers/answersController.js";
+import { db } from "./src/config/dbConfig.js";
 
 // app setup
 const app = express();
 app.listen(6969, () => console.log("Listening on port 6969"));
 
-//cors
+// cors
 app.use(cors(corsOptions));
 
 // middleware
 app.use(express.json());
+
+// db connection
+db.run().catch(console.dir);
 
 // routes
 app.post("/api/v1/chat", semanticSearchController.searchAction);
